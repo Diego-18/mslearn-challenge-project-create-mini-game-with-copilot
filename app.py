dc9a54d806bd5198cc0f878e6f47ae9cc0acd455
Diego Jose Chavez Chirinos <ingdiegochavez18@gmail.com>
2024-05-29 20:43:26 +0000

    run-game-for-console

--- app.py
+++ app.py
@@ -1,69 +1,44 @@
-# Import the necessary libraries
-from flask import Flask, request, jsonify
 import random
 
+def game():
+    player_score = 0
+    computer_score = 0
+    while True:
+        print("Welcome to Rock, Paper, Scissors!")
+        player_choice = input("Enter rock, paper, or scissors: ")
+        computer_choice = random.choice(["rock", "paper", "scissors"])
+        print(f"The computer chose: {computer_choice}")
+        if player_choice == computer_choice:
+            print("It's a tie!")
+        elif player_choice == "rock":
+            if computer_choice == "scissors":
+                print("You win!")
+                player_score += 1
+            else:
+                print("You lose!")
+                computer_score += 1
+        elif player_choice == "paper":
+            if computer_choice == "rock":
+                print("You win!")
+                player_score += 1
+            else:
+                print("You lose!")
+                computer_score += 1
+        elif player_choice == "scissors":
+            if computer_choice == "paper":
+                print("You win!")
+                player_score += 1
+            else:
+                print("You lose!")
+                computer_score += 1
+        else:
+            print("Invalid choice. Please try again.")
+        print(f"Player: {player_score}, Computer: {computer_score}")
+        play_again = input("Play again? (yes/no): ")
+        if play_again != "yes":
+            break
+    print("Thanks for playing!")
 
-# Create a Flask application
-app = Flask(__name__)
-
-# Define the main route
-@app.route('/')
-def index():
-    return 'Welcome to the Rock, Paper, Scissors game!'
-
-# Define the route for the game
-@app.route('/play', methods=['POST'])
-def play():
-    # Check if request.json is None
-    if request.json is None:
-        return jsonify({'error': 'No JSON object provided in the request'}), 400
-
-    # Check if 'choice' key is in request.json
-    if 'choice' not in request.json:
-        return jsonify({'error': 'No choice provided in the JSON object'}), 400
-
-    
-    # Get the user's choice from the request
-    user_choice = request.json['choice']
-
-    # Define the choices
-    choices = ['rock', 'paper', 'scissors']
-
-    # Get the computer's choice
-    computer_choice = random.choice(choices)
-
-    # Determine the winner
-    if user_choice == computer_choice:
-        result = 'It\'s a tie!'
-    elif user_choice == 'rock' and computer_choice == 'scissors':
-        result = 'You win!'
-    elif user_choice == 'paper' and computer_choice == 'rock':
-        result = 'You win!'
-    elif user_choice == 'scissors' and computer_choice == 'paper':
-        result = 'You win!'
-    else:
-        result = 'Computer wins!'
-
-    # Return the result
-    return jsonify({
-        'user_choice': user_choice,
-        'computer_choice': computer_choice,
-        'result': result
-    })
-
-# Run the application
-if __name__ == '__main__':
-    app.run(debug=True)
-
-# To run the application, execute the following command in the terminal:
-# python app.py
-
-# The application will start running on http://
-# You can now make a POST request to http://localhost:5000/play with the following JSON payload:
-# {
-#     "choice": "rock"
-# }
-# The response will contain the user's choice, the computer's choice, and the result of the game.
-
-# You can test the game by making multiple requests with different choices and observing the results.
+if __name__ == "__main__":
+    game()
 
